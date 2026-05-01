864771e00fb079e63a3b7ed7ffc2396a3578a443
Karl Richards <karl.marx.richards@gmail.com>
2026-05-01 15:37:25 -0400

    v1.0

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,17 @@
+export const environment = {
+	production: true,
+	ES_HOST: 'http://localhost:9200',
+	RESULTS_PER_PAGE: 10,
+	redirectUri: 'https://www.zdslogic.com',
+	// ssoUrl: 'http://localhost:18080/auth',
+	ssoUrl: 'https://www.zdslogic.com/keycloak/auth',
+	// apiUrl: 'http://localhost:8082/zdslogic-server/api',
+	booksApiUrl: 'https://www.zdslogic.com/zdslogic-server/api',
+	apiUrl: 'https://www.zdslogic.com/zdslogic-server/api',
+	// wsUrl: 'http://localhost:8082/zdslogic-server/live'
+	wsUrl: 'https://www.zdslogic.com/zdslogic-server/live',
+	socketUrl: 'https://www.zdslogic.com/zdslogic-server/live',
+	originHeader: 'https://www.zdslogic.com',
+	baseURL: 'https://www.zdslogic.com/zdslogic-server/api',
+	siteKey: '6LekHPgZAAAAAF22P6SL0snnu1stiwZkYN-slvea'
+};
